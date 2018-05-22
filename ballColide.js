4e19ae9599b66ebd0391acdbb12dcbf003cdc128
Tim Combs <tim00combs@gmail.com>
2018-05-21 21:09:48 -0700

    initial set up for A-frame game tutorial, add learn.html - ball, paddle, plane made, ball moves, paddle can strike it on camera move, start adding js to control game, utility functions in utilFns.js and recentering ball & scoring in ballCollide.js, have not added these scripts to learn.html yet. add .eslintrc config file

---
+++ ballColide.js
@@ -0,0 +1,25 @@
+let hit = false;
+let resetId = 0;
+const resetBall = () => {
+  clearTimeout(resetId);
+  slct('#ball').body.position.set(0, 0.6, -4);
+  slct('#ball').body.velocity.set(0, 5, 0);
+  slct('#ball').body.angularVelocity.set(0, 0, 0);
+  hit = false;
+  resetId = setTimeout(resetBall, 6000);
+};
+
+let score = 0;
+let hit = false;
+let resetId = 0;
+on(slct('#weapon'), 'collide', (e) => {
+  const ball = slct('#ball');
+  if(e.detail.body.id === ball.body.id && !hit){
+    hit = true;
+    score = score + 1;
+    clearTimeout(resetId);
+    resetId = setTimeout(resetBall, 2000);
+  }
+});
+
+setTimeout(resetBall, 3000);
---
+++ utilFns.js
@@ -0,0 +1,3 @@
+const slct = (sel) => document.querySelector(sel);
+const slctAll = (sel) => document.querySelectorAll(sel);
+const on = (elem, type, hand) => elem.addEventListener(type, hand);
